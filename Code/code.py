2d598ee763d6d25fec744bc7bd63c3df87763056
TrinhNt14 <56534143+TrinhNt14@users.noreply.github.com>
2021-03-24 21:15:03 +0700

    Update code.py

--- Code/code.py
+++ Code/code.py
@@ -1,64 +1,54 @@
-PK
-      ‹mR               Code/PK
-     æ‰CR               Code/.vscode/PK    ŠCR®~ Ë  Ò	     Code/.vscode/settings.json½UKkÜ0¾ò„	¹¤J
-@÷Ö¦É©Kôb(cI^+k[F’½kJþ{G’½k9Û–4{™ñ|£oFšÇþ:=!øKšÞª¾b	¶H$¹]¤éw#´IÓ*ÁÓô}Ó|iúI1(Ót©ÕJC…Kr”×ïÒt [‘¼¢0ÅÕm]íì¬µJ†caÁÇû=B†iÙX—F­øŽb;€qrÆ¥&ççÄ™9Ëe)0ÕÑ´·ü˜Mkï¶6¢b3ž›²PuˆÀùq©r7ÍœýââÅØUö(˜• ‡î@s,‹Ø(½&·Š)x±°Má/åDdöÅöHOhcB—q¸‘›ÒmÖ'‹AN•ræ•"ØFP¶¾'œˆüµR§´(µÁî.DòÛ}zç»Sâ,¨R²ž|è—`¡÷ø:E¾GYÅÉ‹Ï´ïâLá¹)@ûÇÌŒg‡„ãÌp€»lÏÊ‚J®®T©V*r´}#ö®ÖÐg“ÄTž‹‰OøŽ¯ƒÉbT"h#s,(Ñˆ¶¥ô§‚ÅÔ½±à_|T£¾fPy0(QS8ó—!Û) MSNn¡Ì\ÜR=¶Zø&²&nEl=ê$¡”mÂHƒðMƒ,e½úZb!Æþ;º]tkìl8iºbþpì4°µð\ƒ—¨•OIè¡*Ö\¸…²k3K;:H{=EÚc^FÓ®%f÷Ë›¸Ÿ‘ðŠÿ=If=L2ÖfüšzÔ²òÿ'²"LUÒa;¡3e¤íoñ«µ5œ‚PáxÃH†2c¸!=ó:¼"
-ÜÀt~¾sX÷lqa³Oë5¶Ç¼Ä¶š>«óàÉŽyâ4˜Ù›:Ó´4žóÒñOiî•¶ê{-ªÕŒq•øÅþ[>ZášÒõ+Eû™Ë­à4½ûý0!ÊÓéÉÓoPK    zbYR»v^£7  ì     Code/Algorithm.pyV[oÚH~Gâ?±Šb;¤aL€²ª+m¥ìj_öaÕ·ªkx£Øc38þ÷{Ž=öØi1H0sîß¹YfE®KÈx¹è“ˆl÷bûZfÂSþïãà³Ë5H
-4WÏÂ§ø¿ôHðÃñ KdôáXËNÜy
-nP*Š`Ö'Ð£EùªüÉÓ£z7_õ«°¾<oOaÓŠ£ÎÍ™>#ÉzHr3Žl|«ðéT
-•ˆäéu›ÊD\­œ57ÝíˆZ³ôœB<³Þ™áyfÏÒ«pHÛËT ŸkxÈÒÃÃÆA¢&Âáõ	"òå¥tbH¬XµÄÊ!rˆ\e	hG?ùíê­ƒ=9¶ˆÑµQ£Pêù>ÇŒ·w$g¹¨B›Ï"OS®[¨_Py¸ÚhæãQ›)SLÊïÒÀgä¾îÄVýÁVý[âëýn9øn)š­›$ÍŸÑGxx°ÇƒŽ=ï©IUŽç˜2æz¸Jç¨÷Ã0§É“ÖÐés®e¹Ï&ˆZþ¥ü!žR‘	ìë¢Õõº»…Dª=d})JñÏß¾·Xå–z>.Š¾{5hõÛºú'EïV°™}
-I=D év˜WGãù^8	<é7ÔƒAHWE1t†„j$ØØrØpêÛ‚Š¡¦©è¶â¹¯¨+Ž `nð>Ž!-øËU€4¡»˜¡T(¯ã¹ìŒ©°ŽkPw½[üÔwÌã}ÎcœÅžâ™À!Ì"
-—­^º„¨þÁ^yÚ3.¡/±!¦F¸m;[sÄÔMøëÝò[ÃÀÕÑŽ[ÊÒvÏ5%‰”ôâQâ-&R¼Ía´ÐÙ¿¯õZÆ|·;Š"ëÔ}¿þÈÞ]4TPœAÐHö@&F6 ¥	,Äû±×äë¿ÿ3ñýÉT\W±?„>ö6Y·•Üt–r7‹YÑa×kÐ{PC[3á…èp²§¢R•‡—"kÜÏòäøÓ©ÈNÉK™+ZÜj
-™B›ÿ±B6ukÊ¼!lT8Ä	7­ ÓÑTBÜwáßä÷‹ENª½bßjíóê?Ëbb?õÞDÒ¿xÆS¯˜Â¾§à7-ö´ž¨¤å()T’’áç4•lìNgJÅ~¥æÅª©Â6ÐËä• ÍÈo*µÂU…æ…Kî ®#Ž	ÐIg\ª8ž˜ð
-ÊÕŒ³¥k_ðraÓÌÂðã£!Ó¨g&»f
-Ü–h÷v ×z½˜¯-$óÇÅr±Úbj<ußÑ›,œ?.Tßr÷×üæ±%žIÒî®µµ…)—ã2Ê«é|=]±ë˜›>ÎûÜ×-æËpÎp«1¶^-ØSÇ³Õr²ÕÏB,\â¯ëÓp‹´#ÿ´)®1º\|dËÅ”6ç=Ô¯L¾±ÿ?PK    åeYRjª-î’  d     Code/Convert.py­“]kƒ0†ïÿÃYÇ ¶vÓ¤v³ë]–"Ö¦5ØE‰)]÷ë—“˜Z?»X@Ì9yÏ×cdŸU)$ÔR0~p×ÙÑ=dùéB9©½•ë€Z;–É$=Vyº¥bm“oVö˜ÖcÉ©ª¨ÈÒšú R~ $ðayžg’pzþP±“‰1÷¥ Œ·Ú£.ik^Cf1vG:=¬ë5Ûlº‰—·WPy\ºŽ,å—d«:Ld™ìhFjÐ´5Ù^›ÇªñÆ÷oóã*TpÐš‚Ö]GŠ!2}0íó6~ëž§Ó¦0©Q >l}%•Èøè±¦«Á1¢µ¨Ì¡Á©"IK•ð¿á¼!9°OÈÞ\çœ)wŠœ·Ò¾!•øZdÍáA•ßÀ¬Ò.®tžžÔùoHnçÂÙ2N¸7¢]µe&Çµa4çôK‹]ç^QÃæÂ æ/¸Y _ÍÐ¢W/	Å<-m «÷’,Ñ…zþ0Âº•ÀüýŸfòöú>1WÛJúa<GVá:?PK    â}nRë—U  Æ     Code/DAAA.py­VK£F¾[ò¨Œ7Zp°DCfœXáèH¹åä²Úm íi‰‡ƒÁŸªnƒ=ãÕZ6Ýt}UõÕƒÂ+Ø¬7å±ÌŽ»¿ÿú}óËr±\¬`_—"‹Eû*Jd,x<9æ…,_Òå"-âùÅç„¡½[. ?	?—…H¹Dyá@¿… xxF°í¨jh…ÚQ:¸4z× Ôu€©¯«Aÿ½ÈDÆZ_3þ¬«¼”"+×64 ±¼¤yl&nÑ¶XG‚vÝ@çl²z@èÕ@¢¬ÑÈB”U‘M=ã-š^ƒµa À9ü.ˆä,€ÙÝ^†¦X—/âIT%¼ðãÄ]2»ˆâ,¾êê¥Tñ‹…eK»ª
-KK@gGø! Öj<ü‘d&Ëæa­ àIýÃ“Jì‹"/FA×ð#¤D6Î«+{¨²¨”yÖ¶W=kUc4ø;a×tdK‘ž èÂð`­QK#ƒ™ýš6ôá­Y–Oi­¬×àÙðpÖ
-d#YBRø=+Vµfêb š –ú6^Òº·jCóÛ¢ñ8žIžö¨=\½q&ˆ¸;„]3
-ºö€g14ê¦ñ:ìÛ™{_öf3x_©]¯²ªubï õZ×–á}%õ¾CI©ˆº‹Ÿ±ª–i•”Ò£ÈDÁË¼ª¹"3~`áüsíþTu M±^±=—)šhh™Pžt¨ÛCŽ³ GŽÊL²?y»Ï=‰.Ý2>²¶ê%‹ì;æ™q*’^y¬Ì¦Ê-Áèy7j³sKkô€¨ä]Qšä©Ëôˆ¸jÊ©/øŠ·½½ê–»:eB¬k¸Ú)¯(ÍÈ‡>·Û¤‹SšžÈq©éÄ5qãN™4Ê¼>›êïD}bï(ÊuäZ¹5O`þ7µ ‹oÐön4îë–§÷Ýyz·xÞh`ÏŒpÐÑÝY“6ºo
-îzhwÅru~Ó³Z¾ˆªâ"vö»æ‹‡ïÖ/>¾Ó<·Æ+óé•cXíùþÒ£<Ð±z±Ó^Íßúß§¶|*d*KyÁ].³,tÎôÔïŸNÌãÇ-Þ‡Zv]¦NN…Ò|xÜþ‰¢g:F·¯gˆ
-hm·Ë‘zÂ•(3oK¢‘slÏí¯J£uf~rài0jÈ÷“úÿ³ºŽ5fJó
-Ë“úk˜üPK    0~nRnÜíyg  Å     Code/genPrime.py…RQo‚0~7ñ?|Ë²…f,®È„úº_°wÃfÁªRjôÁ¿B‹w	p×û¾»ïÊeª, R¹6QT¥ÒmdžœûÈ¹n¢¡ëédÍ3ˆzU)QpOúØ1Äd9À˜}‹Œ!@©¬7¿äS\”Ä·:pž0PAÐP>î)_é¾¾pj„u•q%ÞAmxÜˆ=7‡¯ ·ej¼±f™³™Qi2Ó|!ÑííˆCLÁ®÷á¾íG®€“TåÑK}(“u2"3É§f8Ã·nKvª7¶Å@úA¶HP?æºN>‚¡€ÆFÙ½c×¶7W‡®âM÷¿Þ®RÎ%W©æv¡V¿fR±6±·ç2×²D¿V˜»Š¢Kž<Š$=oÿÎ ƒÖÕhWy(~¸ê[^Û…îNÉR_7¿òáì}‡ÿoš[-Ó‰Jíë¡BÌ3,ªoNƒhFq‡‹ùg†4Œ(!PK    Z€xRì~Þ  ,     Code/Module.py]SíŽ¢0ýOâ;œŒÙlqqHvv4Á1.)Pµ-L©3°O¿½¥Lzï¹§÷ëÀ}ŠÅßu²–Ã?®uNïðEoã•ýžÊŠñEH!Ng.@¬>]‹ Gâæ\U¹TŸB·ÂÝÛ.ØÓ%–6OœÝ'žN÷ûu–;Äc:Ô^^PL˜°>Ä‘ùna]=¹n¬m¶•ín
-¸™Æ@ï†¶gLf¦óä÷Îv‡ÇdZ˜›VØW‘[dz”Š…[œ’ë)áRÂnðÞªx=Æ“˜ui¾_|#ü,ºïxòºæª#Ûù„¼6µ6h–êt×ÖoÁŠ*Âõ®ªB6ô¬ÂÉWût{ðÝívŒ@7‰ÝÔ_‚D+¬hWkig…æê$Xá"(wèi/`j/È2ê%³è½ë°rÂ)½_•9cµBê1FQ:ºÑ´ôql«P²Ùl"¤¿ÿlÂÐ-°’¥Éù¥9óBdƒÏþÉ†=ÚO_ÃhÜã3oK)ó[Ó]òV<’Ð{#Lþ.zöÉ/4Ì÷eX0‚Eo‚¶2«÷,¸¶lâË±lZÍpióæ³iï‘y²ªZ´ê§èdkž¨=úüPK    µhRw’ä9  ‰     Code/Prime.pyu’MjÃ0…÷ßa $’XÎOp—=Aw¥ÕÇ¢±ìŒezýZ(j¥¸ÑBÈÌ¿ož¤š®%4uš¤É+8¡9t¤<T²4-õLÍ'ß§	Œ+ªAïiâ*ßµ:#85<‚€¢€åµkÒ9—]‡úÈÿ\[X€ðÿ®ZJI}B–ÏÆ³a–{Þ_ÈxFOÍ@ðÀöKÝbÝE³ŠóX9eT±À}©Ê^@fwü ~`B3ŽõþŠÊË/wILóý¿	‰IB.œ0U1ý›
-À«<÷!½Ñ€–¤#k²d›çm¾›õj»ùn•»èœvú´²¥]‚óPK    kaRø:ÓB1  ‚     Code/shanks.pyuSÁŽÓ0½Wê?<iJ–´´{ 	±½€‹X8­º’›8[ÇÎ:NÛì×3S·M³€/‰3oÞ¼™yQeeG)|1W˜{é„W[‰/I½²Þ":m´ð’!Ñhc¼Beð#ÒvE÷á “9*»+mí´	ªøÃp :NÖ¸Å Fy%´z–üµÑ> öß(	ó»Ê¨ÝUå¡j”ÖÉ ¼‚/„uOÐ¬®
-‘]¡´DÔb†É©pÈ˜çh™ÅfY‚’jªJ·D¿S¾è©à£ræxé%G×DÄkìý_i‰¹öXJÈ­40v×E[J$]³0‚ÃííMææ£ýV'}ãW›ùç>°AN£X).),©?0Z<nˆt‰ˆ¶2Æí¿æßñ-l)SuJüòÎ®„£!”Ñ1÷Ô³¡de|ÄÆ×OŽÞb¼¡™„ðVèFäa² Õ%C¸÷´(­„Fž“”È\«˜_ï¾Þ$‹Vì'ÌJF&Á$Á¨7òÁÃÑO$OQ%Ã¬m8èˆÖDýÅ:Ï)dÚuLK“!-dº¹„2QjµV5Í¸¤ãåt2Ö,ixUçÎqòÙ™6¥Üj,Ç	7N4?Oíãè³Ð#á/Í&›­‹sóaÝGQ‡ÜY_úBºC–V4ö¥Ôv‡’c}?‰3ðc·îËsô!z>Mƒ?;ò*£²rl’¿Ý4¥mNß¿Kp“èÿÄáøPK    §CDR´æL†   É      Code/test.jsU]
-Â0„ß¹ÃR(¤B%)‘2^D|©¤ÈTÁ€xw›Dgaù˜f¹S‚9Ë4aZWöôn_ùf!ÐÔúO2 Q2OÃÙÙ´Ð#Ùäºû¾£'g´ÊÎ$ÂÑž ¨Ÿ™• Ò¦twÔ’¯‡g¾OE¿\Éœc¸Eg¶.^DêÞPK    DRƒ üŒ  ³     Code/test.txt}PíJÄ0ü_è;,áN[.›¯M„ú(BcO=Ñ"Ø¼½Ù¦gsph³3;™ýh Jè!<vXW3ÜAˆ‰Ó÷ò4î†ÂžSLÌ)˜e¬Š²®êj<>oÚûº‚tfLi±Æ\s±ˆå¦Éÿï×ÓÛ<€X}ø|ri8 lÜ´p7%5@_Âà´ÁÜ6tÉí6u·%òk"‰¥í%
-f.ŒcÆË¾àw²éôþµÛ¼:¹ç½5ËÖP*m,9_-”lÞ¯ÒÛ:g­F¥’MßXX’ž<^³>®VÈVvb¾…PË»¿úŸ»Ún®‘TÆZc‘<×*  gµ×Æxå-)%5zI×ä=:²JœG‘†|$iÿPK    etxRs´Ê       Code/test1.pyUMÚ0½GÊ˜²ªä°aH ‰Ãª[U½lÕÃª(B˜Å˜(ÉjI}=v>Uµ@¶Çofž_Æ¶8¦§¬€¼È„|uá;Û²­–@=lcÛºƒô1ËâRÙBæÂØê«D¶Õ[ l6W¶}uSãCºÃ9³­=vØVÞ8ž¿¿ü¸0øöüô‚ý×_/?uÿôøc!“„oáxJÞœ¬]Š¢³°-PMªk!‰tÚ¹Ù"êN‹­gäe†ééƒgèŸáhLÛS„„,–¯œx.8Æ®³a[ 2,—¨¡½Õ:9ÃÐdpÚðÝ¬Ä†C`DÆ‹÷LÂ7½æªVí[‹è‚ú¥N-I.äŽõÑ¼¸¹	êÂÔëRLÕkçˆC¼‰t±Ñ(/\EzDøòÞ0*@/àMÚWm*ë¥Ti&dA„£ëK{{bÓ`î8f³	ß¬ŠÓ*–åjçœ¨*À¾Ž(¶zŠY˜ßÉ¢uSRÂ²ªï‡8ß±zOSžm”GÍx®PƒAkùØ‰"á“æ®míjëS'	¥ú”ÌàúH]Š¨óÛ…ê{7`~èîcqì–z;»Æž6Åm,õ¯Á;~Ö`£u-2
-®t'ùÿiˆM±êŽsòG¤ä¶òn{Ì˜ï8Nhß¯P#rÇpQâxHsg¤*Ýükž]ÿû¥æ<îaØ'æê4G}=âé¿¾SÎ¯EÔõ[Ie±®ñ:#5´óú¤Ýi÷óC¬4‘	¹Ô]el«=çjV²ë°ç&bÉêDÀ°¹;ÕâÂÏ.Þ9iŠ^]3{Ò^ÚR	[2}Nï€Î2u!œMf“9>ó9«G!ð&z‚I0¥ÓÈø'×<KÖMnñÄdé(ÑK¦†Kbh„cŸyLa&t<f>HÌófJ²±7«’7)“&cãâ6i”þPK    ¹{iRLÉÖ,7  .     Code/test2.pyXmoÛ6þ ÿsP@ríV¤(J2æZtA—hW ˜á²­$jlY±UÞ¯ß_$RrÚ¦hbòž;Þ=¼;^Rlªíî@ö‡]QÞŸŸ­ò[²Ù®žÖ¹·‘rD6þäüŒÀWI¦dQ”^é·ërÆ&s{9™Œ©Þ¨aƒªÕö[¾Cuò‚lÔÖívG
-R”d—•w¹ŒÈ:GÛæ4ü*n‰WÎŠ9™NÑC:Rs†W“¡:ÁoÍÛ§zêÃpH˜…Øå‡§]Ijú‚º÷Ï˜â"	$ÃUV“—dqù
-þß-W^6"E¸!1èGÍFQGfxìMV®nŠòß|·Ï•žŽ ¦ ôgÖRu¤ÖgÖbJu~Tß¿Ýëý)#¯_“E»·“{/ì­ŒLíå–»v©¢!c°6G[
-OŽ@Gà qÃA`\µÀõQ­%¤‰¯ô%?éZdp¥s³ÕHQÏæê*+Håƒ×åž±!KF„²Ø÷Õí¬òåÍa ãÍ"ä:þô'¸ÄÜcÂò#[W÷Ù"?©®šWÙ~Y7OU•ï– aŸï5´;êÒ¼’ü2Åèä^+muÌ!³î‰9y‰)¯š‰V IiÀùÚŽcÒÚí`“>v»<œÆRÑßçµ+®ÉH8ðîíû\ŸâzU,7á¸öþ+*ï4ó£¶™0áû~kè¡-$Òlmtº¶¢½?¦#¢þ?mý—Séópø@†®£³=ô¬¹«ñ€xú½{Úç}1‘U†Ê}QÞ{Æ¡
-ÂˆÃ”	*’0	y"ÂP¤©Î©êr·Ë°fgP"NRÆQ:"ç<<¢¦UwÂ‡˜©S{«UŸ®¯ˆW'tØ)š¤Aóú0K¿ŽHåËléÀ›,z°àÞÔíø¥Ê»høøøé²¥C6O‘HJ"G,L9…ïJü(Å@CÆišJE ˜>8SbG±ˆAž„!ØáIªÄªÚ¿ÓV¼ŠŽ©?ôñÇLø3Õ‡P£Dði'ŸŸµÎ/€à’êôÔgË'€Å‚ÇIœpF	ç”ÇZoâX„Lˆ8)†L° x°(†€\ª½­Pƒ'”2€&a$,€ôHcMŠYÊÓ8¢aJ1ž¡—(~dø°2ÛóÀ}æ›(e{sõáÝç7W³‰OÈ‰¢‡²tÌÕÊZ­w/ÈuFî·úÛwå25ÉT#½š8]4)süÊ?¹úÒ\¥‘2³‡/Ãîxè¨+sPŒ§ô5ÆR r¶A^ü¤G*.‚WM¹Qéš@²&‘€
-Nƒƒ8æ<áë'4IÒ(q@c*0 ê£`‚‡a¢/Úh¾+²‚lœáÂ¼¢Ý7ÒpÑ†§Z­«û¾È7äpÿdÖæ"S‰qHrb–ÇPhOÁ× 
-2^Dà#£?&ÌMã¬B÷¶Ï½kQ ï_v÷$…Fä3Ôsv®ø®_Ò’é	A½@ðøs=*¹©8x€ÈàïACqÓêçfëô„­ž5Þ1(µ_eð–+o?Û¯‹¥ìÞœÂý¹å_}úŒ½´à­½n(LÎtóc6w*`ö“Ð÷¦v‘æ3íË”u`Ï8…S—¨q½Ô<EŠúyÛät.[½»º¼"×—NV‡gÈÝÓ¸~·Gõ,UÇÞéÞPœjTZ±âyõöšüõûç¾ïŸzìòMÕÖfÑÔÕ}Ò*hÀíXÄrå¥Ó}ß5c†|}—m²õOÎeøÛWD!…T %'&ªŸz9Eâã¬–0OfmìRiƒ³Ôù™™l#Ù>¡ …¦ªƒ5Ý
-	‰1è¼I
-ÃxÃ@`
-„i"L“€G	Œ:ÐŽ£¤™\V¨æAŸŸÑ9yƒÛ°i_fÁÎgØH*›Tu~fÝ}Ír8øóýõå‡A»ù£áÀà$§½àðTûöÙšª¿ŒÚ¡RC·Ôìé q|éýnXa$8¹5ZŽêèc†ÚŒì¦:ˆå®õŒÕr05VÄH9©"i@¬i±´ÖAý‘ßâïBž´:”zÖHÈÇâîþÐ#®v‰SþêƒŒRsMF›PðßÿPK    \GiR÷ë¯ª  7     Code/test3.pyVmoÛ6þÀÿáæ €ä*­H½ó€|(Ò Ë—¥†^ ÛjÂ6–ÉYåýúÝ‰¢DJNS±Å»‡Çç^)±+öåªC)òûÉÙäl›}Ý~ûü˜Ykrvö|røÉak‘[¹Ý¯ó%Ÿ¯ôå|~ÁZA&‹ý÷¬¤íðvRôe_‚ ‘C™æ÷™å:ð˜‘mu}Ä°ò¥XÁbA-fhÕV3y‚Ý›×OµäÃl\C”Ùá¹Ì¡&§ÏáÈ‰Þ?ŒôKA€”Vioa}Dýÿï7[+u`m“Š†xÔÜB¹Š#bïÒ|{'ò³²Êä¾Öƒš!ÔmŸyª#ÓžyÉåQô(¿¿?ˆÇ#ú¸ZPžïßÃº—•@²7º(……¾\ã²ì—Ò¸@k3$Ú+¤{­â¨)$	ùUk Zåº	#tžQÚqÿ¯àÚó¡EŽ¹È.·Ž=_ÉTXÊk{Îg<v€ñÈ¶ev¶Ùæî°GÐñn"k~íyÇ‚–T{<Ôx¤ÅCºÎ°h»æ]Zm„¸{.Š¬Üà-ðY…¨é´—È¤Y9ü² ïY¯í÷¨C–9æ‡+xK
-Ù4¥š‡R}P:pö¨û1õ†6°ñ»ßNcY8?du–±VA¦€cÜ­êçb½›ÃpZ[ÿ‰Â:y§&<´m»7ô­o$è‚¬	ÓˆFQe_0ä¿â©ï»h8Ïfß`f]V8³VæŽo„g?ÊS•ƒH…¬BE¡,.Ë2¥&\bÁ{DXÐïÉo7ŒV*Þ•È,‰v Pô^¢kÑbÛí8*_	/-‚ º‹B88c1JTŒ_é¼¦(ðî -
-Ôé$[˜­NÙÅ¢« ?n/-…)šyÆ‘—ð0ð£€{‰Ïð[ªŸu$,ò¸Ï’8d,tCÞœJ5‚(ŒP{ÚñãDª×\NyczX¿`öÌz¢MØK®î=Â#£ž+%-Éf»aˆüÂ0à<˜Ï¢D‘$5Oü$
-˜— .à~âzI$Õi³›G¸;Žb?ô‚Ø÷™µ>¬ÙI’L’d’$ÓHþ‰ÑEÉ&gRÖÌ¦ß/o®®?ü0URšý'ºûirÖçeJ°©u+=‡›öíU‰ûü/X¨ò¨‰¾K0{r¦i`jžðXß9ïðÙ­i±Ù9B!ÕòxÐ`bÖ
-Ó#Æ¤q©sl1ÚFJg:¤ú=ÿIžçÒ[lZ×e>ÖF„…c™ÆA˜p/¹ç…žE¾û.ÇIà…‘Ë"R]xØœ¡	ßóÜ„ÄTHjç•HìŒ·uM/A“ÞÍ&÷š¯r÷'‘íàðð¬Ö*å	:Å}¬[æQáFØd®Ÿ [›‹8%g¯‡Ì,EW¢GbÅz” 	úqÚ‡'õeôBpÌb¹…¶EFÑEäsû /¼ßýöm@éUsâÓ¿§]ˆ»á¿R¢Ó/Ñòæò›ÝïR¼+ó­U-«G±i&·ÀS|{¥ñ«OŸQ5£µ²7œÂ¬š¾>–+£–?Ùc6µA¤1¯Èô·R:( Yr
-'“ØâF¥y*¤2(HôË¶áÕZ¾º¾¼†›K£‹Ã2ótŸ?žVKj«aïôtèÒ
-§IcEóçÓõ‡øüñÏ1÷ÛQôÙ®è{St}õ:§v0ghT^Øtú0Í‘æåƒÔßÿPK    {IiR©†1~  ©     Code/test4.pyUÿŽÚ0þ‰wðn:)-eGs;8Uë¤=b¨?ä€´KË®ÝÓÏnZ’ÂiZ%¶¿Äög;ÈsYèªZKµŸN¦“\ìà\ä—“`i *€³M'€‚R©˜ò¬¬Ö<Ú¸bÍÃ^Ñ "4Ë²xš¶Ã#œjWh èDí[ptöà¹¦ÖrqL²pd|°|ãÁ³Ç»^™Yø>p¡E}Ñ
-Jú3´œÂû9I _"’’f¶hÏñ»Ïr–zd"E‡¡8 ¡Z>ðˆØm¢ò­T¿…®„Ù×gÐ„]ôkn©jCgÍ-FW´4ï÷ƒ<	dô;,R~žž µ:Ýé]U±+¦(j+šl`Ž§ù¨5˜ôzCëºFHRŒ”Wã HnÜÑ×Ì¨ì¸ÿ,¼èöDŽµP£®óÀPÏ7¦”%¶rÍn¹çÜç¯„|åy¦:¹È¶u v›&ˆÀ^§_/ºFA"õ_:q$§ò¤¢†¸Ÿš/I•I¹½”¥Ðîpˆ¢¬Æ)øSvÎZížÁÉZaÝør32Œ‘ÝÀQ©ùÒzR®`qróˆî€î@°¯÷Ø"«?Æ†Ë{ðA4Øp=L„#ï¬ú?®s™Õ[‡p’ÙY²™ìeÂ—žçÙƒŽvàJ²£¸¹è*ª¼y€ùqºûgq³ïÁº®ðÎÚŒw	þ«N•¸'‘y Š¨,hÐ®±áŸXaC¿|}6ïÅrµø®¤:0ƒ Â¿ÉS{Xžß³òFxsÒ	òúG!¼c‘%jÆ7ò×5ÅüêÀa&tñxÔé1S,»´éóPK    É‚nRŠ.…ùn  ç     Code/test5.py­VMsÚ0½3ÃØÂd"'Ávm¦œ:=ôÖS/†ÈXP%`ˆ0©=þ÷îZ–%ÀÚ”!ÈÙ}»ûöC©œ,r™¥2Ì§)"€$áwÝàk!6¹–K¡P¯ha"Ù ˜ÕZƒ"¨lð(ÍS‰Ða aõÐÏj!³:VK<ö¼]åJf¹š£C’ª—å*eÎÅ)ÚÜÁ,QÕç5Ø`Ôé%bN‰ºÒ µÌ·:Ûl\€]‡ f |‚!ÈÅFBÈ­ßF$¾ºÛévú0‹év!r	˜'>iPÙ‹Ôù@ êÊQÄ°qKÛ·y@	åÝæÄ 0ÜÞ ¬-¼z_³™ÊT^öœªZ(¤õ],¶ò‹Ö+½“v°$ºéj›`ogÛlš«Uf)ÊétbT¬À9(ño“céær¹†‘M$‚A8ªÌ F7~Hœ@F¸AoŒÅTÚ"„Á "W 8*‡¨‘ELÐM…ÁÎaõáJ f”1ÇÏÒ‡ÔáYcjqÝ8‘¦­e@ySƒ"Â3Ú­QºÄ‹Ò."Y
-eõOYìëµ;¯~­5ü»:ÒÐeU˜ÒÖ´þ­2ªq^S£77Õ4ÑÌñûQõr¹]ä*€¹Ì¤ùJïvÓšS8\ƒCnÅÕ¸1´šI¾D%-zª’›Ù
-×Ê ·KTfØðûènlØR+ªóexÉ«IbÆ:òq^€ÐIåâ„}Hö»d±a´sç½œêúìQcxj }Kí‚¼š“ÇzŒïbeÔ°ká½‘Û!·èP½×}7~¤n¹h`2:£Þ-ýSBÇÓ9;—WnØó7ú¦Jh]Ô”„Ö‚–üúí	"OÐÜ-²¹dØP¶¾Æv{‹+à
-BëÉP¦êÖÜypãÁØ7‚âmV	îûvlï	2FWªEÝ«ñÞ|VòøxJC¯àV¬É"¦µŒëLá™´Ð­À*«ƒß¼Ð×ø†ñ}æ}Xk•å¬Î€¬Æ¼…~lèû°ãŽö™Ï¾KÏx¼y’%2º³jzª™˜“&ôšËLTË²*LàÈÖ±­“GWÖh¯¬Ð\¨½Eÿ8÷o»T&­G~¨I´O”­ÀøÑ‡n'Áóc·CtÞÇ¤ðî†ùaœõR%—=ú~ly ½Ïbó;òPK 
-      ‹mR             $              Code/
-         `ñþ®ó×òzË»³ ×QAé>úÖPK 
-     æ‰CR             $          #   Code/.vscode/
-         j7vúÖù R»³ ×÷Ê3vúÖPK     ŠCR®~ Ë  Ò	   $           N   Code/.vscode/settings.json
-         ¤–Ì‡úÖƒº»³ ×j7vúÖPK     zbYR»v^£7  ì   $          Q  Code/Algorithm.py
-         @<ÚØ5×‘_Ö³ × 9."×PK     åeYRjª-î’  d   $           ·  Code/Convert.py
-         Æ s©9×<ƒaÖ³ ×–‰§FúÖPK     â}nRë—U  Æ   $           v	  Code/DAAA.py
-         gZœ®×¦0cÖ³ ×ü‹(fï×PK     0~nRnÜíyg  Å   $           /  Code/genPrime.py
-         OGgô®×%dÖ³ ×q jwü×PK     Z€xRì~Þ  ,   $           Ä  Code/Module.py
-          yŒ ×,>ñ³ ×%5Z–ŒúÖPK     µhRw’ä9  ‰   $           Î  Code/Prime.py
-         óQÝZ1×FzeÖ³ ×½˜ÆK5×PK     kaRø:ÓB1  ‚   $           ÿ  Code/shanks.py
-         Þmc×ÃNgÖ³ ×DÁ<õ×PK     §CDR´æL†   É    $           \  Code/test.js
-         ûë&•úÖu"Ä•} ×ážúÖPK     DRƒ üŒ  ³   $             Code/test.txt
-         R
-4OúÖÍóæ–} ×v¡.OúÖPK     etxRs´Ê     $           V  Code/test1.py
-         °€8€ ×9#iÖ³ × ®To×PK     ¹{iRLÉÖ,7  .   $           K  Code/test2.py
-         À_¾×9#iÖ³ ×´rYŽ×PK     \GiR÷ë¯ª  7   $           ­  Code/test3.py
-         ›(SÃ‡×EJiÖ³ ×Ëý³8×PK     {IiR©†1~  ©   $           å$  Code/test4.py
-         ’žH“‰×[jÖ³ ×wõÖ9×PK     É‚nRŠ.…ùn  ç   $           Ž'  Code/test5.py
-         mlˆ³×—‚jÖ³ ×`±Ùë×PK      ]  '+    
+# y2 = b^-1
+# b^-1 mod a
+# ax + by = d = gcd(a, b)
+# d = a
+# x = x2, y = y2
+
+def gcd_and_inverse(a, b):
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    y2 = 0
+    
+    while b > 0:
+        q = a // b
+        r = a % b
+        a = b
+        b = r
+        x = x2 - q * x1
+        y = y2 - q * y1
+        x2 = x1
+        y2 = y1
+        x1 = x
+        y1 = y
+    d = a    
+    return [d, x2, y2]
+# bin(): he 10 -> he 2
+# int(): bat ky he nao -> he 10
+# oct(): he 10 -> he 8
+# hex(): he 10 -> he 16
+
+# b^-1 mod a => y2 = b^-1
+import string
+
+def module(b, n, m):
+    n = bin(n)
+    n = n[2:]
+    n = n[::-1]
+    x = 1
+    power = b % m
+    for i in range(0, len(n)):
+        if (n[i] == str(1)):
+            x = (x * power) % m
+        power = (power ** 2) % m
+    return x
+
+print(module(2, 1999, 2579))
+
+# dict_alphabet = dict(zip(range(0, 26), string.ascii_uppercase))
+
+# def get_key(val):
+#     for key, value in dict_alphabet.items():
+#         if val == value:
+#             return key
+#     return "key doesn't exist"
